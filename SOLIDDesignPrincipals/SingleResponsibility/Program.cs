94009e229a2c2450a44f06868640e5ad3794dca2
AB <AndrewBraunsdorf@gmail.com>
2019-12-26 18:03:26 -0500

    create text file with journal entries

--- SOLIDDesignPrincipals/SingleResponsibility/Program.cs
+++ SOLIDDesignPrincipals/SingleResponsibility/Program.cs
@@ -55,10 +55,21 @@ namespace ResponsibilityPrincipal
             if (overwrite || !File.Exists(filename))
                 File.WriteAllText(filename, journal.ToString());
         }
+    }
+
     public class Demo
     {
         static void Main(string[] args)
         {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename);
+            Process.Start(filename);
         }
     }
 }
