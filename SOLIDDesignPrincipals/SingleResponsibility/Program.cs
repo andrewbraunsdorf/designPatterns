b3073886a9b7152976c2d7352124a00919a59f23
AB <AndrewBraunsdorf@gmail.com>
2019-12-26 18:02:44 -0500

    added persisting objects logic

--- SOLIDDesignPrincipals/SingleResponsibility/Program.cs
+++ SOLIDDesignPrincipals/SingleResponsibility/Program.cs
@@ -47,7 +47,14 @@ namespace ResponsibilityPrincipal
         }
     }
 
-
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
     public class Demo
     {
         static void Main(string[] args)
