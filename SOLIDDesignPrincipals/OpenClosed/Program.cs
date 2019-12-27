b3ee7b441e4183d5b7bbe7a1bb53f382c86787aa
AB <AndrewBraunsdorf@gmail.com>
2019-12-27 18:59:44 -0500

    added enums for Open Closed

---
+++ SOLIDDesignPrincipals/OpenClosed/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace OpenClosed
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    }
+
+      public class Demo
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
