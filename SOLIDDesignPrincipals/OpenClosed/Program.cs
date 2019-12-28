60546b800d62ed0ca7f44e64c9beb8bc6cc53dbe
AB <AndrewBraunsdorf@gmail.com>
2019-12-27 19:01:35 -0500

    added more product filters

--- SOLIDDesignPrincipals/OpenClosed/Program.cs
+++ SOLIDDesignPrincipals/OpenClosed/Program.cs
@@ -36,6 +36,19 @@ namespace OpenClosed
                 if (p.Color == color)
                     yield return p;
         }
+                public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        }
     }
       public class Demo
     {
