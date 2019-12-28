90afc4982f057abe917d77d2c695cb2ec998be23
AB <AndrewBraunsdorf@gmail.com>
2019-12-28 12:08:48 -0500

    added and specs

--- SOLIDDesignPrincipals/OpenClosed/Program.cs
+++ SOLIDDesignPrincipals/OpenClosed/Program.cs
@@ -90,6 +90,22 @@ namespace OpenClosed
             return p.Size == size;
         }
     }
+
+        public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return first.IsSatisfied(p) && second.IsSatisfied(p);
+        }
+    }
       public class Demo
     {
         static void Main(string[] args)
