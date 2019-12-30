f9abbf1ebafebbcfbd7122d8aae555a3c75972dc
AB <AndrewBraunsdorf@gmail.com>
2019-12-29 23:35:50 -0500

    used better filter

--- SOLIDDesignPrincipals/OpenClosed/Program.cs
+++ SOLIDDesignPrincipals/OpenClosed/Program.cs
@@ -131,6 +131,24 @@ namespace OpenClosed
             WriteLine("Green products:");
             foreach (var p in pf.FilterByColor(products, Color.Green))
                 WriteLine($" - {p.Name} is green");
+
+            var bf = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                WriteLine($" - {p.Name} is green");
+
+            WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+              new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+
         }
     }
 }
