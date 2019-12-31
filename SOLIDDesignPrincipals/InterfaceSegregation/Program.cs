c8fbba3e1ef044824c94d85f5a01064c507425ba
AB <AndrewBraunsdorf@gmail.com>
2019-12-31 16:02:20 -0500

    add documents

---
+++ SOLIDDesignPrincipals/InterfaceSegregation/Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace InterfaceSegregation
+{
+    public class Document
+    {
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+
+    // ok if you need a multifunction machine
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
