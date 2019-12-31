66a2a951add2efd8b988832bf333ab0d1249fd9d
AB <AndrewBraunsdorf@gmail.com>
2019-12-30 20:02:23 -0500

    find area of square

--- SOLIDDesignPrincipals/LiskovSubstitution/Program.cs
+++ SOLIDDesignPrincipals/LiskovSubstitution/Program.cs
@@ -50,6 +50,11 @@ namespace DotNetDesignPatternDemos.SOLID.LiskovSubstitutionPrinciple
         {
             Rectangle rc = new Rectangle(2, 3);
             WriteLine($"{rc} has area {Area(rc)}");
+
+            ectangle sq = new Square();
+            sq.Width = 4;
+            WriteLine($"{sq} has area {Area(sq)}");
+            
             Console.ReadLine();
         }
     }
