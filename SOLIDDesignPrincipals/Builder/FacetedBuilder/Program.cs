5e2b8abafc6d8c608cfd69c60c83cccf0982ae53
AB <AndrewBraunsdorf@gmail.com>
2020-01-05 18:34:24 -0500

    added PersonBuilder

--- SOLIDDesignPrincipals/Builder/FacetedBuilder/Program.cs
+++ SOLIDDesignPrincipals/Builder/FacetedBuilder/Program.cs
@@ -15,6 +15,19 @@ namespace FacetedBuilder
         }
     }
 
+    public class PersonBuilder
+    {
+        protected Person person = new Person();
+
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+          return pb.person;
+        }
+    }
+
 
     class Program
     {
