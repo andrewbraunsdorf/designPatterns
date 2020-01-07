583f5a7854c797d0967ffb7dd077d65ecfa255dd
AB <AndrewBraunsdorf@gmail.com>
2020-01-06 20:05:35 -0500

    added main logic

--- SOLIDDesignPrincipals/Builder/FacetedBuilder/Program.cs
+++ SOLIDDesignPrincipals/Builder/FacetedBuilder/Program.cs
@@ -82,7 +82,19 @@ namespace FacetedBuilder
     {
         static void Main(string[] args)
         {
+            var pb = new PersonBuilder();
+            Person person = pb
+              .Lives
+                .At("123 London Road")
+                .In("London")
+                .WithPostcode("SW12BC")
+              .Works
+                .At("Fabrikam")
+                .AsA("Engineer")
+                .Earning(123000);
 
+            Console.WriteLine(person);
+            Console.ReadLine();
         }
     }
 }
