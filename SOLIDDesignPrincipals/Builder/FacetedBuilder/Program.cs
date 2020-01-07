e1a06e771fec25a59bd83a9ee00e7d609f3016b8
AB <AndrewBraunsdorf@gmail.com>
2020-01-06 20:04:35 -0500

    Added person address builder

--- SOLIDDesignPrincipals/Builder/FacetedBuilder/Program.cs
+++ SOLIDDesignPrincipals/Builder/FacetedBuilder/Program.cs
@@ -52,7 +52,32 @@ namespace FacetedBuilder
             return this;
         }
     }
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
 
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
