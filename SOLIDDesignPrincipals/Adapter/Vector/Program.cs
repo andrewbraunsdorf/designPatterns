38826484b6b0db05f68da90400933e6050f7242b
AB <AndrewBraunsdorf@gmail.com>
2020-01-16 19:28:00 -0500

    added vectorrectangel

--- SOLIDDesignPrincipals/Adapter/Vector/Program.cs
+++ SOLIDDesignPrincipals/Adapter/Vector/Program.cs
@@ -33,6 +33,17 @@ namespace Vector
         }
     }
 
+        public class VectorRectangle : VectorObject
+    {
+        public VectorRectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+
     public class VectorObject : Collection<Line>
     {
 
