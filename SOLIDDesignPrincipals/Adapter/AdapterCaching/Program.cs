ce45490e2cc9f988462defd971c357872d2dbf9c
AB <AndrewBraunsdorf@gmail.com>
2020-01-20 21:03:47 -0500

    added line

--- SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
+++ SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
@@ -82,6 +82,23 @@ namespace Adapter
         }
     }
 
+    public class Line
+    {
+        public Point Start;
+        public Point End;
+
+        public Line(Point start, Point end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        protected bool Equals(Line other)
+        {
+            return Equals(Start, other.Start) && Equals(End, other.End);
+        }
+    }    
+
 
 
     class Program
