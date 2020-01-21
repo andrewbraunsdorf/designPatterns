74e8156a43018f6562717d32ef4249a50d09d339
AB <AndrewBraunsdorf@gmail.com>
2020-01-20 21:04:15 -0500

    added more line

--- SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
+++ SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
@@ -97,7 +97,22 @@ namespace Adapter
         {
             return Equals(Start, other.Start) && Equals(End, other.End);
         }
-    }    
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Line)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Start != null ? Start.GetHashCode() : 0) * 397) ^ (End != null ? End.GetHashCode() : 0);
+            }
+        }
+    }
 
 
 
