0486d2aeab0f615a8f0b5c0ebbe84a7018a34178
AB <AndrewBraunsdorf@gmail.com>
2020-01-20 20:56:27 -0500

    add caching points

---
+++ SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using static System.Console;
+
+namespace Adapter
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        protected bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point)obj);
+        }
+
+        
+    }
+
+
+    class Program
+    {
+        private static void Draw()
+        {
+        }
+    }
+}
