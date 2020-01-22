6bbbfc8a0adcf6da0afb015f6ef43b49886e28e1
AB <AndrewBraunsdorf@gmail.com>
2020-01-21 20:11:32 -0500

    added vector rectangle

--- SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
+++ SOLIDDesignPrincipals/Adapter/AdapterCaching/Program.cs
@@ -114,6 +114,21 @@ namespace Adapter
         }
     }
 
+    public abstract class VectorObject : Collection<Line>
+    {
+    }
+
+    public class VectorRectangle : VectorObject
+    {
+        public VectorRectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+
 
 
     class Program
