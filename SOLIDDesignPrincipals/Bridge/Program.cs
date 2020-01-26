6399f916ffb7d0bf66a0984fbb1fa6303b402fbb
AB <AndrewBraunsdorf@gmail.com>
2020-01-25 20:31:00 -0500

    added irenderer and vector renderer

---
+++ SOLIDDesignPrincipals/Bridge/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Bridge
+{
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            Console.WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
