d80c7448c12bc0f8e15e26b6bd111524db8a468b
AB <AndrewBraunsdorf@gmail.com>
2020-01-29 20:12:26 -0500

    added neuron class

--- SOLIDDesignPrincipals/Composite/NeuralNetworks/Program.cs
+++ SOLIDDesignPrincipals/Composite/NeuralNetworks/Program.cs
@@ -19,6 +19,22 @@ namespace NeuralNetworks
         }
     }
 
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return this;
+        }
+    }
+
 
     class Program
     {
