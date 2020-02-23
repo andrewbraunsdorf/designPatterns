16902b23c5e11a1b8c413f98716c225f1387f927
AB <AndrewBraunsdorf@gmail.com>
2020-02-23 14:34:38 -0500

    added bird

---
+++ SOLIDDesignPrincipals/Decorator/MultipleInheritances/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritances
+{
+    public class Bird
+    {
+        public void Fly()
+        {
+
+        }
+    }
+
+    public class Lizard
+    {
+        public void Crawl()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
