37c19a6f8a4eab1b7335fda7b826228e6a0ed4f7
AB <AndrewBraunsdorf@gmail.com>
2020-02-24 22:20:12 -0500

    added Shape

---
+++ SOLIDDesignPrincipals/Decorator/Dynamic/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DynamicDecorator
+{
+    public abstract class Shape
+    {
+        public virtual string AsString() => string.Empty;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
